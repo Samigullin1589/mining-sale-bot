bbfa954c4a82005fa017c96bfbf357997ad6abd4
Samigullin1589 <98953304+Samigullin1589@users.noreply.github.com>
2025-06-15 21:54:55 +0400

    Update mining_bot.py

--- mining_bot.py
+++ mining_bot.py
@@ -102,5 +102,5 @@ def post_news():
     except Exception as e:
         return f'Ошибка при получении новостей: {str(e)}'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
